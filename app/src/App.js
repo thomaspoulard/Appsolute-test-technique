bcba7686499a0d0b8d30424b677b1e4aaeae8bae
POULARD Thomas <pro.thomaspoulard@gmail.com>
2021-06-03 21:16:36 +0200

    feat: fetch articles with useState and useEffect

--- app/src/App.js
+++ app/src/App.js
@@ -1,63 +1,11 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { ArticleList } from './components/ArticleList/ArticleList'
 
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      isLoaded: false
-    }
-  }
-
-  componentDidMount() {
-
-    // Get all articles id from the uri
-    fetch('https://hacker-news.firebaseio.com/v0/newstories.json')
-      .then(res => res.json())
-      .then(articlesId => {
-        const articlePromiseList = articlesId.slice(-10)
-          .reverse()
-          .map(articleId => {
-            return fetch(`https://hacker-news.firebaseio.com/v0/item/${articleId}.json`)
-              .then(res => res.json())
-          });
-
-        Promise.all(articlePromiseList)
-          .then(articleList => {
-            this.setState({
-              isLoaded: true,
-              articles: articleList
-            })
-
-          })
-      });
-  }
-
-  render() {
-
-    const { isLoaded, articles } = this.state;
-
-    // While data fetched from the API is loading, displaying a loading message
-    if (!isLoaded) {
-      return <div>Data is loading....</div>;
-    }
-
-    // Executes once data from the API has been loaded
-      return (
-        <div className="App">
-          Les données ont été chargées avec succès!
-
-          <ul>
-            {articles.map(article => (
-              <li>{article.title}</li>
-            ))}
-          </ul>
-
-        </div>
-      );
-    }
+const App = () => {
+  return (
+    <ArticleList/>
+  )
 }
 
-export default App;
+export default App
---
+++ app/src/components/ArticleList/ArticleList.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+
+export const ArticleList = () => {
+
+  const [articles, setArticles] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('https://hacker-news.firebaseio.com/v0/newstories.json')
+      .then(res => res.json())
+      .then(articlesId => {
+        const articlePromiseList = articlesId.slice(-10)
+          .reverse()
+          .map(articleId => {
+            return fetch(`https://hacker-news.firebaseio.com/v0/item/${articleId}.json`)
+              .then(res => res.json())
+          })
+
+        Promise.all(articlePromiseList)
+          .then(articleList => {
+            setArticles(articleList)
+            setIsLoading(false)
+          })
+      })
+
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>Data is loading....</div>
+    )
+  }
+
+  return (
+    <div>
+      Les données ont été chargées avec succès!
+
+      <ul>
+        {articles.map(article => (
+          <li>{article.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
