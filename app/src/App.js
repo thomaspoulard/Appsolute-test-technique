719289729a9670fcbc091d8acdbf537f3c0fe7d6
POULARD Thomas <pro.thomaspoulard@gmail.com>
2021-06-03 17:05:13 +0200

    feat: get last 10 articleId

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
+let articleId;
+
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
       articlesIdList: [],
+      article: [],
       isLoaded: false
     }
   }
 
   componentDidMount() {
+
+    // Get all articles id from the uri
     fetch('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty')
       .then(res => res.json())
       .then(json => {
@@ -20,22 +25,26 @@ class App extends Component {
           articlesIdList: json
         })
       });
+
+      fetch(`https://hacker-news.firebaseio.com/v0/item/${articleId}.json?print=pretty`)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          article: json
+        })
+      });
   }
 
   render() {
 
     let { isLoaded, articlesIdList } = this.state;
 
-    // Select the last 10 id of the whole articlesIdList
-    {articlesIdList.slice(-10).reverse().map(articleId => {
-      let articleUri = `https://hacker-news.firebaseio.com/v0/item/${articleId}.json?print=pretty`;
 
-      console.log(articleUri);
-
+    // Select the last 10 id and push the values into a new array
+    {articlesIdList.slice(-10).reverse().forEach(currentArticleId => {
+      articleId = currentArticleId;
     })}
-
-
-
 
     // RETOUR AFFICHAGE
 
@@ -47,9 +56,12 @@ class App extends Component {
     // Executes once data from the API has been loaded
     else {
       return (
-          <div className="App">
-            Les données ont été chargées avec succès!
-          </div>
+        <div className="App">
+          Les données ont été chargées avec succès!
+        <br/>
+        <br/>
+          <div>{articleId}</div>
+        </div>
       );
     }
   }
