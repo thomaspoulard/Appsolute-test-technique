50c9548d087ce68a28d79af11ba2ce1724185f47
POULARD Thomas <pro.thomaspoulard@gmail.com>
2021-06-03 10:24:18 +0200

    log articlesIdList in console

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const articlesIdList = [];
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    }
+  }
+
+  // Récupérer les données en json avec l'url, on les stocke dans la var ''items''
+  componentDidMount() {
+    fetch('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json
+        })
+      });
+  }
+
+  render() {
+
+    var { isLoaded, items } = this.state;
+
+    // On ajoute chaque élément du tableau items dans la variable articlesIdList
+    {items.forEach(element => {
+      articlesIdList.push(element)
+    })}
+
+    // Tant que les données du fetch ne peuvent pas s'afficher, on affiche un message de chargement
+    if (!isLoaded) {
+      return <div>Data is loading....</div>;
+    }
+
+    else {
+      return (
+          <div className="App">Les données ont été chargées avec succès!</div>
+      );
+    }
+  }
 }
+
+console.log(articlesIdList);
 
 export default App;
