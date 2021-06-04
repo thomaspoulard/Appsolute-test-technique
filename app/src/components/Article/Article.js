883a1471b86292f239dddb60851de2531df794f8
POULARD Thomas <pro.thomaspoulard@gmail.com>
2021-06-04 08:26:34 +0200

    feat: article component

--- app/src/components/Article/Article.js
+++ app/src/components/Article/Article.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import {styled} from '@material-ui/core/styles'
 
-export const Article = () => {
+const Container = styled('div')({
+  display: 'flex',
+  fontFamily: 'roboto, sans-serif',
+  cursor: 'pointer',
+  alignItems: 'center',
+  justifyContent: 'center',
+  background: '#F6F6EF'
+})
 
+const Content = styled('div')({
+  display: 'flex',
+  flexGrow: 1,
+  flexDirection: 'column'
+})
+
+const StyledTitle = styled('div')({
+  fontSize: 16
+})
+
+const Subtitle = styled(StyledTitle)({
+  fontSize: 14,
+  color: 'green'
+})
+
+const Link = styled('a')({
+  color: 'black',
+  textDecoration: 'none'
+})
+
+export const Article = (props) => {
   return (
-    <div>
-      Article Component
-    </div>
+      <Container>
+        <Content>
+          <Link href={props.url}><StyledTitle>{props.children}</StyledTitle></Link>
+          <Subtitle>{props.subtitle}</Subtitle>
+          <br/>
+        </Content>
+      </Container>
   )
-
 }
