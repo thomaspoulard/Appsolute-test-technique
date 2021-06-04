af8b929a0e5ce910250748f6bd820e35c6489467
POULARD Thomas <pro.thomaspoulard@gmail.com>
2021-06-04 08:51:50 +0200

    feat: add separator component

---
+++ app/src/components/Separator/Separator.js
@@ -0,0 +1,9 @@
+import {styled} from '@material-ui/core/styles'
+
+export const Separator = styled('div')({
+  height: 0,
+  width: 500,
+  border: '1px solid black',
+  opacity: '25%',
+  background: '#F6F6EF'
+})
