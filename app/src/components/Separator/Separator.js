67b181e6928fb71c182ac1cf465bb5afa489b61a
POULARD Thomas <pro.thomaspoulard@gmail.com>
2021-06-08 16:58:21 +0200

    feat: stylize separator

---
+++ app/src/components/Separator/Separator.js
@@ -0,0 +1,10 @@
+import {styled} from '@material-ui/core/styles'
+
+export const Separator = styled('div')({
+  display: 'flex',
+  width: 287,
+  border: '1px solid black',
+  opacity: '12%',
+  backgroundColor: 'black',
+  margin: '10px 0 0 -10px'
+})
